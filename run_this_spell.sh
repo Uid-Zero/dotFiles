94589c6590b17c956988ef0ac82ff68319aeb3bc
Sai Shanmukha Kolavennu <Shanmukha.Kolavennu@gmail.com>
2019-06-03 18:11:12 -0400

    Firefox..... Gone :(

--- run_this_spell.sh
+++ run_this_spell.sh
@@ -7,8 +7,3 @@
 
 ln -s --force "$PWD"/bashrc ~/.bashrc
 ln -s --force "$PWD"/bash_profile ~/.bash_profile
-
-# Force link Firefox.js to the system wide Mozilla Firefox configuration under /etc
-# I only have 1 user (me) using my Work-Station so this does not effect me. If you have multiple users, than you can either (i) Make this a system wide configuration or (ii) Force link this file under ~/.mozilla/firefox/<**default**>/user.js file - This will only effect you.
-
-sudo ln -s --force  "$PWD"/firefox.js /etc/firefox/pref/firefox.js
