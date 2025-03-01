c1ac7fc9c70f77fc9500653f06a0b77a087057c2
Sai Kolavennu <Shanmukha.Kolavennu@gMail.com>
2025-03-01 11:37:09 -0600

    Update 2025.

--- run_this_spell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make this file executable.
-chmod 754 run_this_spell.sh
-
-# Force link BashRC and Bash_Profile files to the files with same name in Home directory.
-
-ln -s --force "$PWD"/bashrc ~/.bashrc
-ln -s --force "$PWD"/bash_profile ~/.bash_profile
